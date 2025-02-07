e063e3e95d85bbf4e768964f6fd0e00abb581bfb
kykyryzka0-o <maksholenko12@gmail.com>
2025-02-06 21:29:05 -0600

    Update HelloworldApplication.java
    second commit

--- src/main/java/com/holenko/helloworld/HelloworldApplication.java
+++ src/main/java/com/holenko/helloworld/HelloworldApplication.java
@@ -9,6 +9,7 @@ public class HelloworldApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HelloworldApplication.class, args);
 		System.out.println("Hello, World!");
+		System.out.println("my name is max");
 	}
 
 }
